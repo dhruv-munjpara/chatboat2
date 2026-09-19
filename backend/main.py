73bac7f6e6a309d6426b462aca0f7132eb705fc2
dhruv-munjpara <dhruvmunjpara1511@gmail.com>
2026-09-20 02:58:32 +0530

    2.5

--- backend/main.py
+++ backend/main.py
@@ -356,7 +356,7 @@ def delete(
 # =========================
 
 llm = ChatGoogleGenerativeAI(
-    model="gemini-1.5-flash",
+    model="gemini-2.5-flash",
     google_api_key=os.getenv("GEMINI_API_KEY"),
     temperature=0.8
 )
